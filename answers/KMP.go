54aa14d5890cdfe4770b2ab05c6979d3f97dffea
KenmyZhang <1027837952@qq.com>
2018-03-25 16:13:07 +0800

    KMP

--- answers/KMP.go
+++ answers/KMP.go
@@ -12,9 +12,14 @@ func main() {
 	fmt.Println(KMPSearch(str, pattern, next))
 	str = "anmincd"
 	pattern = "abcde"
+	CalNextVal(pattern, next)
 	fmt.Println(KMPSearch(str, pattern, next))		
 }
-
+/*假设现在文本串S匹配到 i 位置，模式串P匹配到 j 位置
+  如果j = -1，或者当前字符匹配成功（即S[i] == P[j]），都令i++，j++，继续匹配下一个字符；
+  如果j != -1，且当前字符匹配失败（即S[i] != P[j]），则令 i 不变，j = next[j]。 此举意
+  味着失配时，模式串P相对于文本串S向右移动了j - next [j] 位。换言之，当匹配失败时，模式串向
+  右移动的位数为：失配字符所在位置 - 失配字符对应的next 值，即移动的实际位数为：j - next[j]，且此值大于等于1。*/
 func KMPSearch(str, pattern string, next []int) int {
 	i, j := 0, 0
 	sLen, pLen := len(str), len(pattern)
@@ -37,14 +42,34 @@ func KMPSearch(str, pattern string, next []int) int {
 func CalNext(str string, next []int) {
 	strLen := len(str)
 	next[0] = -1
-	k, j := -1, 0
+	j, k := 0, -1
 	for ; j < strLen - 1; {
 		if k == -1 || str[j] == str[k] {
 			k++
 			j++
 			next[j] = k
 		} else {
-			k = next[k]
+			k = next[k]			
+		}
+	}
+}
+
+//
+func CalNextVal(str string, next []int) {
+	strLen := len(str)
+	next[0] = -1
+	j, k := 0, -1
+	for ; j < strLen - 1; {
+		if k == -1 || str[j] == str[k] {
+			k++
+			j++
+			if str[j] != str[k] {
+				next[j] = k
+			} else {
+				next[j] = next[k]
+			}
+		} else {
+			k = next[k]			
 		}
 	}
 }
