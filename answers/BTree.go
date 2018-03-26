5d2a75d7dcc11cce3112b4a1c81f12091a1bfab4
KenmyZhang <1027837952@qq.com>
2018-03-26 21:38:52 +0800

    二叉树

---
+++ answers/BTree.go
@@ -0,0 +1,97 @@
+package main 
+
+import (
+	"fmt"
+)
+
+type BTree struct {
+	Data string
+	Left *BTree
+	Right *BTree
+}
+
+func main() {
+	var bTree *BTree 
+	bTree = CreateBinaryTree()
+	fmt.Println("Create binary tree success!")
+	fmt.Println("Total Node of tree:", TotalNumOfNode(bTree))
+	fmt.Println("Depth of tree:", DepthOfTree(bTree))
+	fmt.Println("Leaf num::", LeafNum(bTree))
+	fmt.Println("PreRange")
+	PreRange(bTree)
+	fmt.Println("MidRange")
+	MidRange(bTree)
+	fmt.Println("LastRange")
+	LastRange(bTree)
+}
+
+func CreateBinaryTree() *BTree {
+	var bTree *BTree
+	var data string
+ 	fmt.Scanln(&data)
+ 	if data == "#" {
+ 		bTree = nil
+ 	} else {
+ 		bTree = &BTree{}
+ 		bTree.Data = data
+ 		bTree.Left = CreateBinaryTree()
+ 		bTree.Right = CreateBinaryTree()
+ 	}
+ 	return bTree
+ }
+
+ func PreRange(bTree *BTree) {
+ 	if bTree != nil {
+ 		fmt.Println(bTree.Data)
+ 		PreRange(bTree.Left)
+ 		PreRange(bTree.Right)
+ 	}
+ }
+
+ func MidRange(bTree *BTree) {
+ 	if bTree != nil {
+ 		MidRange(bTree.Left)
+ 		fmt.Println(bTree.Data)
+ 		MidRange(bTree.Right)
+ 	}
+ }
+
+func LastRange(bTree *BTree) {
+ 	if bTree != nil {
+ 		LastRange(bTree.Left)
+ 		LastRange(bTree.Right)
+ 		fmt.Println(bTree.Data)
+ 	}
+}
+
+func TotalNumOfNode(bTree *BTree) int {
+	if bTree == nil {
+		return 0
+	} else {
+		return 1 + TotalNumOfNode(bTree.Left) + TotalNumOfNode(bTree.Right)
+	}
+}
+
+func DepthOfTree(bTree *BTree) int {
+	if bTree == nil {
+		return 0
+	} else {
+		var dept int
+		if DepthOfTree(bTree.Left) > DepthOfTree(bTree.Right) { 
+		 dept = DepthOfTree(bTree.Left) + 1
+		} else { 
+		 dept = DepthOfTree(bTree.Right) + 1
+		}
+		return dept
+	}
+}
+
+func LeafNum(bTree *BTree) int {
+	if bTree == nil {
+		return 0
+	} else if bTree.Left == nil && bTree.Right == nil {
+		return 1
+	} else {
+		return LeafNum(bTree.Left) + LeafNum(bTree.Right)
+	}
+}
