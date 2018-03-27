1d07f8734247a7dd26b484beb1ef6653db01a13a
KenmyZhang <13544285662@163.com>
2018-03-27 16:20:48 +0800

    update

--- answers/ThreadedBTree.go
+++ answers/ThreadedBTree.go
@@ -5,26 +5,35 @@ import (
 )
 
 type BTree struct {
-	Data int    //节点数据
-	LeftTag, RightTag bool //增加的线索标记
+	Data string    //节点数据
+	LeftTag int  //增加的线索标记
+	RightTag int //增加的线索标记
 	LeftChild  *BTree //当LeftTag=0时LeftChild代表左节点，LeftTag=1时代表没有左节点LeftChild代表的是前驱节点
 	RightChild *BTree //当RightTag=0时RightChild代表右节点，RightTag=1时代表没有右节点RightChild代表的是后继节点
 }
 
 var pre *BTree
 
-func CreateBTree(bTree *BTree) {
-	var str string
-        fmt.Scanf("%s", &str)
-	if str == "#" {
-		bTree = nil
-	} else {
-		bTree = &BTree{}
-		bTree.Data = str
-		CreateBTree(bTree.LeftChild)
-		CreateBTree(bTree.RightChild)
-	}
+func main() {
+	t := CreateBinaryTree()
+	h := InOrderThreading(t)
+	InOrderTraverse_Thr(h)
 }
+
+func CreateBinaryTree() *BTree {
+	var bTree *BTree
+	var data string
+	fmt.Scan(&data)
+ 	if data == "#" {
+ 		bTree = nil
+ 	} else {
+ 		bTree = &BTree{}
+ 		bTree.Data = data
+ 		bTree.LeftChild = CreateBinaryTree()
+ 		bTree.RightChild = CreateBinaryTree()
+ 	}
+ 	return bTree
+ }
 
 //为了算法的设计方便我们在线索化二叉树上再增加一个头节点。头节点的Data域为空，LeftChild指向没有线索时的根节点，将LeftTag设为0，而RightChild指向某种遍历方式中的最后一个节点，RightTag=1
 func InThreading(bTree *BTree) {
@@ -39,7 +48,7 @@ func InThreading(bTree *BTree) {
 
 		if nil == pre.RightChild {
 			pre.RightTag = 1
-			pre.RightChild = p
+			pre.RightChild = bTree
 		} else {
 			pre.RightTag = 0
 		}
@@ -49,8 +58,8 @@ func InThreading(bTree *BTree) {
 }
 
 //中序线索化
-func InOrderThreading(head *BTree, bTree *BTree) {
-	head = &BTree{}			//建立头节点
+func InOrderThreading(bTree *BTree) *BTree{
+	head := &BTree{}			//建立头节点
 	head.LeftTag = 0		//头节点有左孩子，若树非空，则其左孩子是树根
 	head.RightTag = 1		//头节点的右孩子指针为右线索
 	head.RightChild = head		//初始化时，右指针指向自己
@@ -59,40 +68,66 @@ func InOrderThreading(head *BTree, bTree *BTree) {
 	} else {
 		head.LeftChild = bTree   //头节点的左孩子指向根
 		pre = head		 //pre初值指向头节点
-		InThreading(pTree)       //中序线索化
+		InThreading(bTree)       //中序线索化
 		pre.RightChild = head	 //中序线索化算法结束后，pre为最右节点，pre的右线索指向头节点
 		pre.RightTag = 1 
 		head.RightChild = pre   //头节点的右线索指向pre
 	}
+	return head
 }
 
-//遍历中序线索二叉树
+/*遍历中序线索二叉树
+	1)左子树线索化
+	2)对空指针线索化：
+		①如果p的左孩子为空，则给p加上左线索，将其LTag置为1，让p的左孩子指针指向pre(前驱);
+		②如果pre的右孩子为空，则给pre加上右线索，将其RTag置为1，让pre的右孩子指针指向p(后继);
+	3)将pre指向刚访问过的结点p，即pre = p;
+	4)右子树线索化。
+
+	等于是一个链表的扫描，所以时间复杂度为O(n)
+*/
 func InOrderTraverse_Thr(bTree *BTree) {
 	p := bTree.LeftChild
-	for {
-		if p == bTree {
-			break
-		}
+	for ; p != bTree; {
 		//沿左孩子向下
-		for{
-			if p.LeftTag != 0 {
-				break
-			}
+		for ; p.LeftTag == 0; {
 			p = p.LeftChild
 		}
-		fmt.Println("data", p.Data)  //访问其左子树为空的节点
+		fmt.Print(" ", p.Data)  //访问其左子树为空的节点
 		//沿右线索
-		for {
-			if p.RightTag != 1 || p.RightChild != bTree { 
-				break
-			}
+		for ; p.RightTag == 1 && p.RightChild != bTree; { 
 			p = p.RightChild
-			fmt.Println("data2", p.Data)
+			fmt.Print(" ", p.Data)
 		}
 		p = p.RightChild
 
 	}
-	fmt.Println("data3", p.Data)
+	fmt.Print(" ", p.Data)
 }
 
+//求后继的算法
+func InOrderNext(bTree *BTree) *BTree {
+	if bTree.RightTag == 1 {
+		return bTree.RightChild
+	} else {
+		q := bTree.RightChild
+		for ; q.LeftTag == 0; {
+			q = q.LeftChild
+		}
+		return q
+	}
+}
+
+//求前驱的算法
+func InOrderPre(bTree *BTree) *BTree {
+	if bTree.LeftTag == 1 {
+		return bTree.LeftChild
+	} else {
+		q := bTree.LeftChild
+		for ; q.RightTag == 0 ; {
+			q = q.RightChild
+		}
+		return q
+	}
+}
 
---
+++ questions/ThreadedBTree.md
@@ -0,0 +1,8 @@
+### Description
+    中序线索二叉树
+
+### Example
+    输入:A B C # # D E # G # # F # # #
+    输出:C B E G D F A
+
+[代码链接](https://github.com/KenmyZhang/InterviewQuestionsAndAnswer/blob/master/answers/ThreadedBTree.go)
